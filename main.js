4476d26479dfd04ca051b2fa08c6a14ee424a3f7
AzizJakbaraliyev <azizjakbaraliyev454@gamil.com>
2024-10-30 10:16:54 +0500

    Figma yani versika qismi tugadi

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+const btn1 = document.querySelector(".btn1");
+btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("salom");
+});
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,6 @@
+const btn1 = document.querySelector(".btn1")
+
+btn1?.addEventListener("click", (e) =>{
+    e.preventDefault()
+    console.log("salom")
+})
